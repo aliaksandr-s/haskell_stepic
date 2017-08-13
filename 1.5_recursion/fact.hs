d6c08842c50532496b752058e8e8a08539a71cb2
aliaksandr-s <s-a-n-9@mail.ru>
2017-08-13 20:24:20 +0300

    Add 1.5.3 fibonacci with acc

--- 1.5_recursion/fact.hs
+++ 1.5_recursion/fact.hs
@@ -3,3 +3,9 @@ factorial :: Integer -> Integer
 factorial n | n == 0    = 1 
   	    | n > 0     = n * factorial (n - 1)
             | otherwise = error "arg must be >= 0"
+
+factorial2 n | n >= 0    = helper 1 n
+	     | otherwise = error "arg must be >=0"
+
+helper acc 0 = acc
+helper acc n = helper (acc * n) (n - 1)
--- 1.5_recursion/fibonacci.hs
+++ 1.5_recursion/fibonacci.hs
@@ -1,6 +1,16 @@
 fibonacci :: Integer -> Integer
-
 fibonacci n | n == 0    = 0
-	    | n == 1    = 1
+            | n == 1    = 1
             | n > 0     = fibonacci (n - 1) + fibonacci (n - 2)
             | otherwise = (-1) ^ (-n + 1) * fibonacci (-n)
+
+
+
+fibonacci2 :: Integer -> Integer
+fibonacci2 n = helper n 0 1 
+
+helper :: Integer -> Integer -> Integer -> Integer
+helper cntr acc1 acc2 | cntr == 0 = acc1
+                      | cntr >= 0 = helper (cntr - 1) acc2 (acc1 + acc2)
+                      | otherwise = helper (cntr + 1) (acc2 - acc1) acc1 
+
