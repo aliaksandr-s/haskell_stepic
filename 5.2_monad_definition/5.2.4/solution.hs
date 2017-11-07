0b0051e9e2265106993f6dbeac79db493eb57fc3
aliaksandr-s <s-a-n-9@mail.ru>
2017-11-07 15:45:31 +0300

    Add 5.2.4

---
+++ 5.2_monad_definition/5.2.4/solution.hs
@@ -0,0 +1,23 @@
+import Control.Monad (ap, liftM)
+data Log a = Log [String] a deriving Show
+
+instance Monad Log where
+  return = returnLog
+  (>>=) = bindLog
+
+instance Functor Log where
+  fmap = liftM
+
+instance Applicative Log where
+  pure = return
+  (<*>) = ap
+
+bindLog :: Log a -> (a -> Log b) -> Log b
+bindLog (Log log a) f = Log (log ++ log1) res
+  where Log log1 res = f a
+
+returnLog :: a -> Log a
+returnLog a = Log [] a
+
+execLoggersList :: a -> [a -> Log a] -> Log a
+execLoggersList a fs = foldl (\acc x -> acc >>= x) (return a) fs
