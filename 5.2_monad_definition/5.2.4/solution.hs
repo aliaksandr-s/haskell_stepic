dcac992c7c187d6912c1c79c559cbbb24dd8f1fd
aliaksandr-s <s-a-n-9@mail.ru>
2017-11-12 11:29:48 +0300

    Fix 5.2.4

--- 5.2_monad_definition/5.2.4/solution.hs
+++ 5.2_monad_definition/5.2.4/solution.hs
@@ -1,4 +1,5 @@
 import Control.Monad (ap, liftM)
+import Control.Applicative
 data Log a = Log [String] a deriving Show
 
 instance Monad Log where
@@ -17,7 +18,7 @@ bindLog (Log log a) f = Log (log ++ log1) res
   where Log log1 res = f a
 
 returnLog :: a -> Log a
-returnLog a = Log [] a
+returnLog = Log []
 
 execLoggersList :: a -> [a -> Log a] -> Log a
 execLoggersList a fs = foldl (\acc x -> acc >>= x) (return a) fs
